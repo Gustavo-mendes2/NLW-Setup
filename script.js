191e8ade6d05cb54d80c4757add14b0519490490
gustavo-mendes2 <gustavo568telesmendes@gmail.com>
2023-01-21 17:18:27 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const form = document.querySelector("form")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, 5)
+  if (nlwSetup.dayExists(today)) {
+    alert("true")
+    return
+  } else {
+    nlwSetup.addDay(today)
+  }
+}
+
+function save() {
+  localStorage.setItem("NLWsetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWsetup@habits")) || {}
+
+nlwSetup.setData(data)
+nlwSetup.load()
